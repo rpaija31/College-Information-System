4e03259cc649f6abe4acde589ac522436772855e
Dell <Dell@DESKTOP-KJ8IPE9>
2022-01-04 13:17:31 +0545

    Merge origin/master
    Conflicts:
    	src/main/java/com/mycompany/college_is/Test.java

--- src/main/java/com/mycompany/college_is/Test.java
+++ src/main/java/com/mycompany/college_is/Test.java
@@ -11,6 +11,7 @@ package com.mycompany.college_is;
 public class Test {
     public static void main(String[] args) {
         System.out.println("La suman ke garya");
+        System.out.println("Hello again conflict hai");
     }
     
 }
